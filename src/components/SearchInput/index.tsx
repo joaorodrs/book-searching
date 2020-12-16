d7acc1e0bc810b217e432a098b4b9ea5c1f39c2c
JoaoPaulo-LD <joaopauloalencarrodrigues@gmail.com>
2020-12-16 12:26:26 -0300

    Initial setup finished

---
+++ src/components/SearchInput/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const SearchInput: React.FC = () => {
+  return (
+    <Container>
+      <h1>SearchInput</h1>
+    </Container>
+  )
+}
+
+export default SearchInput
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SearchInput from '../../components/SearchInput'
+
+import { Container } from './styles'
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <SearchInput />
+    </Container>
+  )
+}
+
+export default Home
